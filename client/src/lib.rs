48de17a341e4955b161f462dfb973b8470dd02a8
captainbuckkets <jack.day.27@gmail.com>
2023-12-08 21:26:20 -0700

    Ignore serialized check

--- client/src/lib.rs
+++ client/src/lib.rs
@@ -27,7 +27,8 @@ pub extern crate jsonrpc;
 pub extern crate bitcoincore_rpc_json;
 pub use crate::json::bitcoin;
 pub use bitcoincore_rpc_json as json;
-use json::bitcoin::consensus::{Decodable, ReadExt};
+// use json::bitcoin::consensus::{Decodable, ReadExt};
+use json::bitcoin::consensus::Decodable;
 // use json::bitcoin::hex::HexToBytesIter;
 use json::bitcoin::hashes::hex::HexIterator;
 
@@ -40,14 +41,14 @@ pub use crate::error::Error;
 pub use crate::queryable::*;
 
 fn deserialize_hex<T: Decodable>(hex: &str) -> Result<T> {
-    // let mut reader = HexToBytesIter::new(&hex)?;
     let mut reader = HexIterator::new(&hex)?;
     let object = Decodable::consensus_decode(&mut reader)?;
-    if reader.read_u8().is_ok() {
-        Err(Error::BitcoinSerialization(bitcoin::consensus::encode::Error::ParseFailed(
-            "data not consumed entirely when explicitly deserializing",
-        )))
-    } else {
-        Ok(object)
-    }
+    // if reader.read_u8().is_ok() {
+    //     Err(Error::BitcoinSerialization(bitcoin::consensus::encode::Error::ParseFailed(
+    //         "data not consumed entirely when explicitly deserializing",
+    //     )))
+    // } else {
+    //     Ok(object)
+    // }
+    Ok(object)
 }
